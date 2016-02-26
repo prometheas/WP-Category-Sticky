901ee5154d5128d3e6a8c7154891612b78796abd
John Lianoglou <john.lianoglou@viacom.com>
2016-02-26 18:07:17 -0500

    Bumping the version for a new release

--- bz_category_sticky.php
+++ bz_category_sticky.php
@@ -4,7 +4,7 @@
  Plugin URI: http://www.workinginboxershorts.com/wordpress-custom-taglines
  Description: Set sticky posts for individual category archives
  Author: Brian Zeligson
- Version: 0.13
+ Version: 0.14
  Author URI: http://www.workinginboxershorts.com
 
  ==
